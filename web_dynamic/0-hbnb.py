4342592394ee25baf8a51ebf5a50282556f34235
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-08-09 10:37:22 +0300

    (1) working curl

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+import uuid
+
 from models import storage
 from models.state import State
 from models.city import City
@@ -7,7 +9,10 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
+
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
@@ -34,10 +39,12 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
-                           states=st_ct,
-                           amenities=amenities,
-                           places=places)
+    return render_template(
+        '0-hbnb.html',
+        states=st_ct,
+        amenities=amenities,
+        places=places,
+        cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
